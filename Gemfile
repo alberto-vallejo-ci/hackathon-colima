3b1ac088c431b237f83d3e64746d885432b576ea
Daniel Gaytan <dgaytan6@gmail.com>
2016-08-16 21:49:59 -0500

    Install bootstrap 4.alpha3 and jquery stickyjs

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source "https://rubygems.org"
 ruby "2.3.0"
 gem "rails", "4.2.7"
 
-gem "bootstrap-sass"
 gem "coffee-rails"
 gem "font-awesome-rails"
 gem "jquery-rails"
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
+//= require jquery-sticky/jquery.sticky
 //= require angular-rails-templates
-//= require bootstrap-sprockets
 //= require_tree ./application
 //= require_tree ./templates
