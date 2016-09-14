c6b65cc172d6e048c6922571e6df5836b8c98314
Daniel Gaytan <dgaytan6@gmail.com>
2016-09-13 19:22:15 -0500

    Improve categories list

--- app/assets/javascripts/application/app.es6
+++ app/assets/javascripts/application/app.es6
@@ -1,1 +1,5 @@
 let application = angular.module('application', ['templates', 'ngParallax']);
+
+application.run(['$rootScope', function($rootScope){
+    $('[data-toggle="tooltip"]').tooltip();
+}])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def categories_list(categories)
-    content_tag :ul do
-      categories.each do |category|
-        a = content_tag(:a, image_tag("iconos/#{category.icon}.svg", title: category.name.capitalize, svg: true),
-                        href: '#', data: { value: category.icon })
-        concat content_tag(:li, a)
-      end
-    end
-  end
 end
