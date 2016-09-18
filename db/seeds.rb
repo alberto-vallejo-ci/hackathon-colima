f80db821ca800ddb4f5a2bf4902fab79221c4ff8
Daniel Gaytan <dgaytan6@gmail.com>
2016-09-17 19:59:59 -0500

    Seeds should first_or_create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
 # ---------------------------
-# CATEGORIES
+# All
 # ---------------------------
 
-Category.create(name: 'Manejo integral de residuos sólidos urbanos', icon: 'residuos')
-Category.create(name: 'Ciudad Verde', icon: 'ciudad')
-Category.create(name: 'Arquitectura sustentable', icon: 'arquitectura')
-Category.create(name: 'Innovación tecnológica', icon: 'innovacion')
-Category.create(name: 'Eco tecnologías', icon: 'ecotec')
-Category.create(name: 'Turismo sustentable', icon: 'turismo')
-Category.create(name: 'Energías renovables', icon: 'energia')
-Category.create(name: 'Biodiversidad', icon: 'biodiversidad')
-Category.create(name: 'Educación ambiental', icon: 'educacion')
-Category.create(name: 'Agua', icon: 'agua')
-Category.create(name: 'Sector primario', icon: 'primario')
+Category.where(name: 'Manejo integral de residuos sólidos urbanos', icon: 'residuos').first_or_create
+Category.where(name: 'Ciudad Verde', icon: 'ciudad').first_or_create
+Category.where(name: 'Arquitectura sustentable', icon: 'arquitectura').first_or_create
+Category.where(name: 'Innovación tecnológica', icon: 'innovacion').first_or_create
+Category.where(name: 'Eco tecnologías', icon: 'ecotec').first_or_create
+Category.where(name: 'Turismo sustentable', icon: 'turismo').first_or_create
+Category.where(name: 'Energías renovables', icon: 'energia').first_or_create
+Category.where(name: 'Biodiversidad', icon: 'biodiversidad').first_or_create
+Category.where(name: 'Educación ambiental', icon: 'educacion').first_or_create
+Category.where(name: 'Agua', icon: 'agua').first_or_create
+Category.where(name: 'Sector primario', icon: 'primario').first_or_create
 
 # ---------------------------
-# CHALLENGES
+# Development
 # ---------------------------
 
 if Rails.env.development?
-  Challenge.create(title: 'Save tha world', owner_name: 'User Test', owner_email:' "test@example.com"',
+  Challenge.where(title: 'Save tha world', owner_name: 'User Test', owner_email:' "test@example.com"',
                    owner_phone: '3121234567', description: 'Bacon ipsum dolor amet andouille tenderloin chicken, frankfurter shank porchetta ground round',
                    description_how: 'Bacon ipsum dolor amet andouille tenderloin chicken, frankfurter shank porchetta ground round',
                    is_looking_for_team: true, profile_requirements: 'My requirement', assets: nil,
-                   location: 'Colima', category: Category.first)
+                   location: 'Colima', category: Category.first).first_or_create
 
-  Challenge.create(title: 'Change tha world', owner_name: 'User Test', owner_email:' "test@example.com"',
+  Challenge.where(title: 'Change tha world', owner_name: 'User Test', owner_email:' "test@example.com"',
                    owner_phone: '3121234567', description: 'Bacon ipsum dolor amet andouille tenderloin chicken, frankfurter shank porchetta ground round',
                    description_how: 'Bacon ipsum dolor amet andouille tenderloin chicken, frankfurter shank porchetta ground round',
                    is_looking_for_team: true, profile_requirements: 'My requirement', assets: nil,
-                   location: 'Colima', category: Category.last)
+                   location: 'Colima', category: Category.last).first_or_create
 end
