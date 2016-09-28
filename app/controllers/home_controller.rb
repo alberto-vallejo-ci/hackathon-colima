1936bed88b17920f4fd4c4ee3e7f266990040c1e
Daniel Gaytan <dgaytan6@gmail.com>
2016-09-28 18:37:13 -0500

    FAQ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def show
     @home = ::Home.new
   end
+
+  def faq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: 'home#show'
 
+  get 'informacion', to: 'home#faq'
+
   resource :contact, only: :create
   resource :challenge, only: :create
   resource :registration, only: :show, controller: :registration
